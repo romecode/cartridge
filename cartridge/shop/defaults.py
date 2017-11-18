31987372d5d5c4b9fbb5c406742717aeedfedf8a
focus <focus@focus-desktop>
2017-11-18 14:47:15 -0500

    sort

--- cartridge/shop/defaults.py
+++ cartridge/shop/defaults.py
@@ -276,8 +276,9 @@ register_setting(
         "the options available for sorting a list of products.",
     editable=False,
     default=(
+             (_("Recently added"), "-date_added"),
         (_("Most expensive"), "-unit_price"),  
-        (_("Recently added"), "-date_added"),
+        
         (_("Highest rated"), "-rating_average"),
         (_("Least expensive"), "unit_price"),
         
